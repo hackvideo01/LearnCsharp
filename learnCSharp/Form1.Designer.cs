9961e49e3e2864d9e58618f0432baedad3f56e07
hackvideo01 <hackvideo01@gmail.com>
2021-09-29 16:58:46 +0900

    buom

---
+++ learnCSharp/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace learnCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rdbnt1 = new System.Windows.Forms.RadioButton();
+            this.rdbnt2 = new System.Windows.Forms.RadioButton();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.txbResult = new System.Windows.Forms.TextBox();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.lbRUNSTOPTEXT = new System.Windows.Forms.Label();
+            this.btnRunStop = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(93, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full Name:";
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(232, 47);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(323, 19);
+            this.txbName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Font Type Choose";
+            // 
+            // rdbnt1
+            // 
+            this.rdbnt1.AutoSize = true;
+            this.rdbnt1.Location = new System.Drawing.Point(121, 149);
+            this.rdbnt1.Name = "rdbnt1";
+            this.rdbnt1.Size = new System.Drawing.Size(111, 16);
+            this.rdbnt1.TabIndex = 3;
+            this.rdbnt1.TabStop = true;
+            this.rdbnt1.Text = "lowercase letters";
+            this.rdbnt1.UseVisualStyleBackColor = true;
+            // 
+            // rdbnt2
+            // 
+            this.rdbnt2.AutoSize = true;
+            this.rdbnt2.Location = new System.Drawing.Point(121, 186);
+            this.rdbnt2.Name = "rdbnt2";
+            this.rdbnt2.Size = new System.Drawing.Size(104, 16);
+            this.rdbnt2.TabIndex = 4;
+            this.rdbnt2.TabStop = true;
+            this.rdbnt2.Text = "Capitalize letter";
+            this.rdbnt2.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(435, 78);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(257, 105);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(75, 23);
+            this.btnResult.TabIndex = 6;
+            this.btnResult.Text = "Result";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // txbResult
+            // 
+            this.txbResult.Location = new System.Drawing.Point(338, 107);
+            this.txbResult.Name = "txbResult";
+            this.txbResult.Size = new System.Drawing.Size(312, 19);
+            this.txbResult.TabIndex = 7;
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(516, 78);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(75, 23);
+            this.btnUndo.TabIndex = 8;
+            this.btnUndo.Text = "Undo";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // lbRUNSTOPTEXT
+            // 
+            this.lbRUNSTOPTEXT.AutoSize = true;
+            this.lbRUNSTOPTEXT.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.lbRUNSTOPTEXT.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbRUNSTOPTEXT.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbRUNSTOPTEXT.Location = new System.Drawing.Point(275, 138);
+            this.lbRUNSTOPTEXT.Name = "lbRUNSTOPTEXT";
+            this.lbRUNSTOPTEXT.Padding = new System.Windows.Forms.Padding(3);
+            this.lbRUNSTOPTEXT.Size = new System.Drawing.Size(6, 33);
+            this.lbRUNSTOPTEXT.TabIndex = 9;
+            // 
+            // btnRunStop
+            // 
+            this.btnRunStop.Location = new System.Drawing.Point(257, 186);
+            this.btnRunStop.Name = "btnRunStop";
+            this.btnRunStop.Size = new System.Drawing.Size(75, 23);
+            this.btnRunStop.TabIndex = 10;
+            this.btnRunStop.Text = "START";
+            this.btnRunStop.UseVisualStyleBackColor = true;
+            this.btnRunStop.Click += new System.EventHandler(this.btnRunStop_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(280, 276);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRunStop);
+            this.Controls.Add(this.lbRUNSTOPTEXT);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.txbResult);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.rdbnt2);
+            this.Controls.Add(this.rdbnt1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Change Font Type";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rdbnt1;
+        private System.Windows.Forms.RadioButton rdbnt2;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.TextBox txbResult;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Label lbRUNSTOPTEXT;
+        private System.Windows.Forms.Button btnRunStop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ learnCSharp/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learnCSharp
+{
+    public partial class Form1 : Form
+    {
+        Timer timer1;
+        public Form1()
+        {
+            InitializeComponent();
+
+            CenterToScreen();
+            //this.btnRunStop.Text = "STOP";
+            Point locationOnForm = pictureBox1.FindForm().PointToClient(pictureBox1.Parent.PointToScreen(pictureBox1.Location));
+            timer1 = new Timer();
+            timer1.Tick += timer1_Tick;
+            timer1.Interval = 100;
+        }
+
+        int i = 0;
+        int j = 10;
+        int x = 0;
+        int y = 0;
+        string name = "learnCSharp LEt LEar!";
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++;
+            j+=10;
+           
+            int n = name.Length;
+            if (i < n)
+            {
+                this.lbRUNSTOPTEXT.Text += this.name[i].ToString();
+            }else if (i>=n) 
+            {
+                this.lbRUNSTOPTEXT.ResetText();
+                i = 0;
+            }
+            //this.lbRUNSTOPTEXT.Text = i.ToString();
+            if (i%2==0)
+            {
+                // Get current working directory (..\bin\Debug)
+                string workingDirectory = Environment.CurrentDirectory;
+                // GEt the current PROJECT directory
+                string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+                // Create specific path file
+                string savePathFile1 = string.Format(@"{0}\images\pngwing.jpg", projectDirectory);
+
+                    Bitmap bm1 = new Bitmap(savePathFile1);
+
+                    this.pictureBox1.Image = bm1;
+                    if (x<=0||y<=0)
+                    {
+                        x += 10;
+                        y += 10;
+                        this.pictureBox1.Location = new System.Drawing.Point(x, y);
+                    }
+                    else
+                    {
+                    x = 280 - j;
+                    y = 276 - j;
+                    this.pictureBox1.Location = new System.Drawing.Point(x, y);
+                    }
+                    var leftMostScreen = Screen.AllScreens.OrderBy(s => s.Bounds.Left).First();
+                    var left = leftMostScreen.Bounds.Location;
+            }
+            else
+            {
+                // Get current working directory (..\bin\Debug)
+                string workingDirectory = Environment.CurrentDirectory;
+                // GEt the current PROJECT directory
+                string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+                // Create specific path file
+                string savePathFile2 = string.Format(@"{0}\images\pngwing.com2.png", projectDirectory);
+
+                    Bitmap bm2 = new Bitmap(savePathFile2);
+
+                    this.pictureBox1.Image = bm2;
+            }
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            string name = this.txbName.Text;
+
+            if (this.rdbnt1.Checked == true)
+            {
+                this.txbResult.Text = name.ToLower();
+            }else if (this.rdbnt2.Checked == true)
+            {
+                this.txbResult.Text = name.ToUpper();
+            }
+            else
+            {
+                MessageBox.Show("Checked!!!");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            this.txbResult.Clear();
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            if (this.txbResult.CanUndo == true)
+            {
+                this.txbResult.Undo();
+                this.txbResult.ClearUndo();
+            }
+        }
+
+        private void btnRunStop_Click(object sender, EventArgs e)
+        {
+            //if (this.btnRunStop.Text == "STOP")
+            //{
+            //    this.btnRunStop.Text = "RUN";
+            //}
+            //else
+            //{
+            //    this.btnRunStop.Text = "STOP";
+            //}
+
+
+            timer1.Enabled = !timer1.Enabled;
+            btnRunStop.Text = btnRunStop.Text == "STOP" ? "STRAT" : "STOP";
+        }
+    }
+}
